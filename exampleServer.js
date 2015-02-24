d41bb7d8c6d6162eec9bd96f1d7988099e89bed3
Michael Owens <mowens@ucdavis.edu>
2015-02-24 14:06:23 -0800

    added a write to database feature

--- exampleServer.js
+++ exampleServer.js
@@ -1,25 +1,32 @@
 var http = require("http");
 var url = require("url");
-
+var fs = require("fs");
 var db = [];
+
 
 var router = {
   "/" : function() {
     return "Home";
   },
-  "/submit" : function () {
+  "/submit" : function (request) {
+    writeNewUser(request.parsedUrl, function(){
+      console.log("done writing list");
+    });
+    console.log("returning body now");
     return "Submit";
   }
 };
 
+
 var server = http.createServer(function(request, response){
   var parsedUrl = url.parse(request.url, true);
   db.push(parsedUrl.query);
-  console.log(db);
+  
   var bodyHandler = router[parsedUrl.pathname];
+
   if(bodyHandler) {
     response.statusCode = 200;
-    response.write(bodyHandler());
+    response.write(bodyHandler(request));
     response.end();  
   } else {
     response.statusCode = 404;
@@ -27,8 +34,19 @@ var server = http.createServer(function(request, response){
   }
 });
 
+function writeNewUser(user, callback) {
+  
+   fs.readFile('./db.csv', function (err, fileData) {
+   if (err) throw err;
+   var fileWithConcatenatedData = fileData + user + "\n";
+      fs.writeFile('./db.csv', fileWithConcatenatedData, function(err) {
+        callback(err);
+      });
+  });
+}
 
-// /hello?name=liz&email=liz@tradecrafted.com
+
+
 
 
 server.listen(3000); //listen on port 3000
